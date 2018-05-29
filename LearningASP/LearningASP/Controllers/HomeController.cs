36b63b780cd7e8a330f308c173ee90393feaf2c4
AnhLB <luyenbaoanh@gmail.com>
2018-05-29 22:34:35 +0700

    Return ActionResult

--- LearningASP/LearningASP/Controllers/HomeController.cs
+++ LearningASP/LearningASP/Controllers/HomeController.cs
@@ -9,9 +9,9 @@ namespace LearningASP.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public string Index()
+        public ActionResult Index()
         {
-            return "Hello World";
+            return RedirectToAction("GetAllCustomers", "Customer");
         }
     }
 }
